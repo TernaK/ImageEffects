377470267f3a7b77e92fe12b285b9f0e1ed657d3
Terna Kpamber <t_kpamber@apple.com>
2026-04-23 22:13:40 -0700

    Initial Commit

---
+++ ImageEffects/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ImageEffects
+//
+//  Created by Terna Kpamber on 4/23/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
